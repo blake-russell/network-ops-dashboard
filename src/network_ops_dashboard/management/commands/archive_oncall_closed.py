48ce404e1758e00ad156999d64da9bd33197686c
Blake Russell <30665494+blake-russell@users.noreply.github.com>
2025-09-03 11:58:02 -0500

    Add Cisco Field Notice Response Capability (#15)
    * (feat): Add ability to set status of Impacted or No Impact for Cisco Advisories/Field Notices along with a note for reasoning or remediation steps and progress. On-call auto-archiving enablement will also archive Cisco Field notices that are set to No Impact at the configured time frequency in the on-call module. Added Redis note in changelog.

--- src/network_ops_dashboard/management/commands/archive_oncall_closed.py
+++ src/network_ops_dashboard/management/commands/archive_oncall_closed.py
@@ -2,24 +2,35 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 import logging
 from network_ops_dashboard.oncall.models import OnCallIncident, OnCallSettings
+from network_ops_dashboard.notices.ciscoadvisory.models import CiscoAdvisory
 
 logger = logging.getLogger(f'network_ops_dashboard.{__name__}')
 
 class Command(BaseCommand):
-    help = "Archive all Closed on-call incidents (move to Archived)."
+    help = "Archive all Closed on-call incidents & Cisco Field Notices (move to Archived)."
 
     def handle(self, *args, **kwargs):
         settings_obj = OnCallSettings.load()
         if not settings_obj.auto_archive_enabled:
             logger.info("On-call Archiving is disabled.")
             return
-        qs = OnCallIncident.objects.filter(status="Closed")
+        # On-Call Incidents
+        incident_qs = OnCallIncident.objects.filter(status="Closed")
         now = timezone.now()
-        count = 0
+        inc_count = 0
         logger.info("On-call Archiving Started.")
-        for inc in qs:
+        for inc in incident_qs:
             inc.status = "Archived"
             inc.date_archived = now
             inc.save(update_fields=["status", "date_archived"])
-            count += 1
-        logger.info(f"On-Call archived {count} incident(s).")
+            inc_count += 1
+        logger.info(f"On-Call archived {inc_count} incident(s).")
+        # Cisco Field Notices
+        ciscoadvisory_qs = CiscoAdvisory.objects.filter(status='No Impact')
+        fn_count = 0
+        logger.info("CiscoAdvisory Archiving Started.")
+        for fn in ciscoadvisory_qs:
+            inc.status = "Archived"
+            inc.save(update_fields=["status"])
+            fn_count += 1
+        logger.info(f"CiscoAdvisory archived {fn_count} field notice(s).")
